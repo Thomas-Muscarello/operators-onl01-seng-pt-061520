19d2e5ddbfdf99893cb1b3b13e9d003ecedfe62c
Thomas Muscarello <muscarello.thomas@gmail.com>
2020-06-17 23:14:13 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,16 +1,18 @@
 def unsafe?(speed)
 if speed>60
   puts "true"
+  
 elsif speed<40
   puts "true"
+  
 elsif 40<speed>60
-end
+ end
 end
 
 
 
 def not_safe?(speed)
-	speed ? speed<40:60<speed
+	speed ? speed<40:!60<speed
 end
 	
 
